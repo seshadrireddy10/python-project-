7d7c0ebeb839394d55d97fd89920a8fb1cffa96c
sai <dinkarsai03@gmail.com>
2023-09-11 16:53:22 +0530

    edited test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
+'''This is a module-level docstring that describes the purpose of the app.py module.'''
+
+
 from app import index
 
 
 def test_index():
+    '''This is a module-level docstring that describes the purpose of the app.py module.'''
     assert index() == "Hello Arch world!"
-
